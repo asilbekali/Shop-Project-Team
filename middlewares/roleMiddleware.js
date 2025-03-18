aae7f0486dcd7defb1f54b32621342f4fadc4a32
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-03-18 21:10:02 +0500

    middlewares

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+function roleMiddleware(roles) {
+  return (req, res, next) => {
+    try {
+      const token = req.header("Authorization")?.split(" ")[1];
+      if (!token)
+        return res.status(401).send({ message: "Token not provided" });
+      const user = jwt.verify(token, "apex1");
+      if (!user) return res.status(401).send({ message: "Invalid token" });
+      req.user = user;
+      if (roles.includes(user.role)) {
+        next();
+      } else {
+        res.status(400).send({ message: "Not allowed" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+module.exports = roleMiddleware;
--- routes/region.js
+++ routes/region.js
@@ -1,85 +1,84 @@
 const Region = require("../models/region");
 const { Router } = require("express");
 const { regionVali } = require("../validators/region.validation");
-const { error } = require("winston");
 
 const router = Router();
 
 router.post("/region", async (req, res) => {
-    const { error, value } = regionVali(req.body);
-    if (error) {
-        return res.status(500).send({ message: "Error in validation region" });
-    }
-    const bazaRegion = await Region.findOne({ where: { name: value.name } });
-    if (bazaRegion) {
-        return res.status(409).send({ message: "Region must be uniqe !" });
-    }
-    const newRegion = await Region.create(value);
-    res.send(newRegion);
-    try {
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Error in create region" });
-    }
+  const { error, value } = regionVali(req.body);
+  if (error) {
+    return res.status(500).send({ message: "Error in validation region" });
+  }
+  const bazaRegion = await Region.findOne({ where: { name: value.name } });
+  if (bazaRegion) {
+    return res.status(409).send({ message: "Region must be uniqe !" });
+  }
+  const newRegion = await Region.create(value);
+  res.send(newRegion);
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error in create region" });
+  }
 });
 
 router.get("/regions", async (req, res) => {
-    try {
-        const bazaRegions = await Region.findAll();
-        res.send(bazaRegions);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Error in show regions" });
-    }
+  try {
+    const bazaRegions = await Region.findAll();
+    res.send(bazaRegions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error in show regions" });
+  }
 });
 
 router.patch("/region/:id", async (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    try {
-        const bazaRegions = await Region.findByPk(id);
-        if (!bazaRegions) {
-            return res.status(404).send({ message: "Region nor found !" });
-        }
-
-        await bazaRegions.update({
-            name: name || bazaRegions.name,
-        });
-
-        res.send(bazaRegions);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Error in show regions" });
+  const { id } = req.params;
+  const { name } = req.body;
+  try {
+    const bazaRegions = await Region.findByPk(id);
+    if (!bazaRegions) {
+      return res.status(404).send({ message: "Region nor found !" });
     }
+
+    await bazaRegions.update({
+      name: name || bazaRegions.name,
+    });
+
+    res.send(bazaRegions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error in show regions" });
+  }
 });
 
 router.delete("/region/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        const region = await Region.findByPk(id);
-        if (!region) {
-            return res.status(404).json({ message: "Region not found!" });
-        }
-
-        await region.destroy();
-        res.send({ message: "Region deleted successfully" });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error deleting region",
-            error: error.message,
-        });
+  const { id } = req.params;
+  try {
+    const region = await Region.findByPk(id);
+    if (!region) {
+      return res.status(404).json({ message: "Region not found!" });
     }
+
+    await region.destroy();
+    res.send({ message: "Region deleted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting region",
+      error: error.message,
+    });
+  }
 });
 
 router.get("/region/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        const bazaRegions = await Region.findByPk(id);
-        res.send(bazaRegions);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Error in show regions" });
-    }
+  const { id } = req.params;
+  try {
+    const bazaRegions = await Region.findByPk(id);
+    res.send(bazaRegions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error in show regions" });
+  }
 });
 
 module.exports = router;
