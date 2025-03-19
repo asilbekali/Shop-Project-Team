d0bbb26c513394d0d0cf5df8d85a12bf7b8c4103
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-03-19 14:07:29 +0500

    fixing some issues

--- routes/auth.js
+++ routes/auth.js
@@ -11,139 +11,129 @@ const upload = require("../multer/malter");
 
 const router = require("express").Router();
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "mrasad.apex@gmail.com",
-        pass: "dkxj sako ezqy xczj",
-    },
-});
-
 function genToken(user) {
-    const token = jwt.sign({ id: user.id }, "apex1", { expiresIn: "15m" });
-    return token;
+  const token = jwt.sign({ id: user.id }, "apex1", { expiresIn: "15m" });
+  return token;
 }
 
 function genRefreshToken(user) {
-    const token = jwt.sign({ id: user.id }, "apex2", { expiresIn: "7d" });
-    return token;
+  const token = jwt.sign({ id: user.id }, "apex2", { expiresIn: "7d" });
+  return token;
 }
 
 router.post("/register", upload.single("file"), async (req, res) => {
-    try {
-        const { error } = userValidator.validate(req.body);
-        if (error)
-            return res.status(400).send({ message: error.details[0].message });
-        const file = req.file;
-        logger.info("File received", { file });
-        const imageUrl = `../uploads/${file.filename}`;
+  try {
+    const { error } = userValidator.validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
+    const file = req.file;
+    logger.info("File received", { file });
+    const imageUrl = `../uploads/${file.filename}`;
 
-        const { email, password, phone, ...rest } = req.body;
-        const otp = totp.generate(email + "apex");
-        const hash = bcrypt.hashSync(password, 10);
-        const newUser = await User.create({
-            email,
-            password: hash,
-            status: "pending",
-            phone: phone,
-            image: imageUrl,
-            ...rest,
-        });
-        console.log(otp);
-        sendEmail(email, otp);
-        // sendSMS(phone, otp)                      ///send smm funksiyasini yoqb qoyish kerak
+    const { email, password, phone, ...rest } = req.body;
+    const otp = totp.generate(email + "apex");
+    const hash = bcrypt.hashSync(password, 10);
+    const newUser = await User.create({
+      email,
+      password: hash,
+      status: "pending",
+      phone: phone,
+      image: imageUrl,
+      ...rest,
+    });
+    console.log(otp);
+    sendEmail(email, otp);
+    // sendSMS(phone, otp)                      ///send smm funksiyasini yoqb qoyish kerak
 
-        logger.log("info", `New user registered - ${newUser}`);
-        res.send({ message: "Otp sended to your email" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Something went wrong" });
-    }
+    logger.log("info", `New user registered - ${newUser}`);
+    res.send({ message: "Otp sended to your email" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Something went wrong" });
+  }
 });
 
 router.post("/verify", async (req, res) => {
-    try {
-        const { email, otp } = req.body;
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(404).send({ message: "User not found" });
-        }
-        const match = totp.verify({ token: otp, secret: email + "apex" });
-        if (!match) {
-            return res
-                .status(400)
-                .send({ message: "Code is not valid or expired" });
-        }
-        await user.update({
-            status: "active",
-        });
-        logger.log("info", `User verified - ${user}`);
-        res.send({ message: "Verified" });
-    } catch (error) {
-        console.log(error);
+  try {
+    const { email, otp } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
     }
+    const match = totp.verify({ token: otp, secret: email + "apex" });
+    if (!match) {
+      return res.status(400).send({ message: "Code is not valid or expired" });
+    }
+    await user.update({
+      status: "active",
+    });
+    logger.log("info", `User verified - ${user}`);
+    res.send({ message: "Verified" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/send-otp", async (req, res) => {
-    try {
-        const { email } = req.body;
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(404).send({ message: "User not found" });
-        }
-        if (user.status == "active") {
-            return res
-                .status(200)
-                .send({ message: "Your account is already verified" });
-        }
-        const token = totp.generate(email + "apex");
-        sendEmail(email, token);
-        logger.log("info", `User requested for new otp - ${user.email}`);
-        res.send({ message: "Verification code sended to email" });
-    } catch (error) {
-        console.log(error);
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
     }
+    if (user.status == "active") {
+      return res
+        .status(200)
+        .send({ message: "Your account is already verified" });
+    }
+    const token = totp.generate(email + "apex");
+    sendEmail(email, token);
+    logger.log("info", `User requested for new otp - ${user.email}`);
+    res.send({ message: "Verification code sended to email" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/login", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(400).send({ message: "User not found" });
-        }
-        const match = bcrypt.compareSync(password, user.password);
-        if (!match) {
-            return res.status(401).send({ message: "Password is incorrect" });
-        }
-        if (user.status == "pending") {
-            return res.send({
-                message: "Your account is not verified please verify",
-            });
-        }
-        const access_token = genToken(user);
-        const refresh_token = genRefreshToken(user.email);
-        logger.log("info", `User logged in - ${user}`);
-        res.send({ refresh_token, access_token });
-    } catch (error) {
-        console.log(error);
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).send({ message: "User not found" });
     }
+    const match = bcrypt.compareSync(password, user.password);
+    if (!match) {
+      return res.status(401).send({ message: "Password is incorrect" });
+    }
+    if (user.status == "pending") {
+      return res.send({
+        message: "Your account is not verified please verify",
+      });
+    }
+    const access_token = genToken(user);
+    const refresh_token = genRefreshToken(user.email);
+    logger.log("info", `User logged in - ${user}`);
+    res.send({ refresh_token, access_token });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/access-token", async (req, res) => {
-    try {
-        const { refresh_token } = req.body;
-        const user = jwt.verify(refresh_token, "apex2");
+  try {
+    const { refresh_token } = req.body;
+    const user = jwt.verify(refresh_token, "apex2");
 
-        if (!user)
-            return res.status(401).send({ message: "Invalid refresh token" });
+    if (!user)
+      return res.status(401).send({ message: "Invalid refresh token" });
 
-        logger.log("info", `User got new access_token - ${user.email}`);
-        const access_token = genToken(user);
-        res.send({ access_token });
-    } catch (error) {
-        console.log(error);
-    }
+    logger.log("info", `User got new access_token - ${user.email}`);
+    const access_token = genToken(user);
+    res.send({ access_token });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
