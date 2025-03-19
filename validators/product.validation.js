6d0ee873320f655aa120ee24bb47c7e63fe44e94
asilbekali <asilbekabdugafforov7@gmail.com>
2025-03-19 16:17:20 +0500

    product crud finished

--- validators/product.validation.js
+++ validators/product.validation.js
@@ -6,7 +6,7 @@ function proValid(data) {
         name: Joi.string().min(2).max(50).required(),
         description: Joi.string().min(2).max(150).required(),
         price: Joi.number().required(),
-        category_id: Joi.number().required(),
+        category_Id: Joi.number().required(),
     }).validate(data, { abortEarly: true });
 }
 
