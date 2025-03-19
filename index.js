ced1a5d5fa20efad4f8874b32be558008a0f037f
asilbekali <asilbekabdugafforov7@gmail.com>
2025-03-19 12:13:54 +0500

    category crud finished and user pagenation to fixed

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { connectDb } = require("./config/db");
 const regionRoute = require("./routes/region");
+const authUserRoute = require("./routes/auth")
+const categoryRoute = require("./routes/category")
 const app = express();
 app.use(express.json());
 app.use(regionRoute);
+app.use(authUserRoute)
+app.use(categoryRoute)
 connectDb();
 
 app.listen(3002, () => console.log("server is running on 3001"));
